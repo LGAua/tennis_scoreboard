14eabe63a6638046e8fd55d813c61c6d13d514da
glebl <glebleontyev2003@gmail.com>
2024-09-11 23:31:53 +0200

    Add custom error page

---
+++ src/main/java/com/lga/tennisscoreboard/util/ErrorPage.java
@@ -0,0 +1,22 @@
+package com.lga.tennisscoreboard.util;
+
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.web.IWebExchange;
+import org.thymeleaf.context.WebContext;
+
+import java.io.Writer;
+
+public class ErrorPage {
+
+    public static void sendErrorPage(String errorMessage, HttpServletRequest req, HttpServletResponse resp, ServletContext servletContext, Writer writer) {
+        WebContext webContext = new WebContext(ThymeleafUtil.getWebExchange(req, resp, servletContext));
+
+        String referer = req.getHeader("referer");
+        webContext.setVariable("errorMessage", errorMessage);
+        webContext.setVariable("sendBack", referer);
+
+        ThymeleafUtil.startRenderingPage("error-page", writer, webContext);
+    }
+}
